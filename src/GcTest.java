ec3eb0623a648314c2963f46168b28b979b24e5d
shenshenyu5 <2803752845@qq.com>
2019-08-27 17:24:59 +0800

    first commit

---
+++ src/GcTest.java
@@ -0,0 +1,28 @@
+import java.lang.ref.SoftReference;
+
+public class GcTest {
+    /**
+     * testGC()方法执行后objA和objB会不会被GC呢
+     */
+    public Object instance = null;
+    public static final int _1MB=1024*1024;
+    /**
+     * 这个成员属性的唯一意义就是占点内存，以便能GC日志中看清楚是否被回收过
+     */
+    private byte[] bigSize = new byte[2*_1MB];
+    public static  void testGC(){
+        GcTest objA = new GcTest();
+        GcTest objB = new GcTest();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA =null;
+        objB =null;
+        //假设在这行发生的GC，那么objA和objB是否能被回收呢
+        System.gc();
+    }
+
+    public static void main(String[] args) {
+        E:\JvmLearn\out\production\JvmLearn\GcTest.class
+    }
+}
